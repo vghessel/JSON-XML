1549f72f7814a84ce0d215a3f70bfd048fab8b28
Vinicius Hessel <vinighessel@outlook.com>
2021-12-03 21:10:10 -0300

    Json2XML

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
+import xml.etree.cElementTree as ET
 
 ceps = ["06764480", "04139000"]
 retorno = []
@@ -7,4 +8,20 @@ for cep in ceps:
     cep_body = request.json()
     retorno.append(cep_body)
 
-print(retorno)
+
+for data in retorno:
+    root = ET.Element("root")
+
+    ET.SubElement(root, "cep").text = data["cep"]
+    ET.SubElement(root, "logradouro").text = data["logradouro"]
+    ET.SubElement(root, "complemento").text = data["complemento"]
+    ET.SubElement(root, "bairro").text = data["bairro"]
+    ET.SubElement(root, "localidade").text = data["localidade"]
+    ET.SubElement(root, "uf").text = data["uf"]
+    ET.SubElement(root, "ibge").text = data["ibge"]
+    ET.SubElement(root, "gia").text = data["gia"]
+    ET.SubElement(root, "ddd").text = data["ddd"]
+    ET.SubElement(root, "siafi").text = data["siafi"]
+
+tree = ET.ElementTree(root)
+tree.write("CEP.xml")
